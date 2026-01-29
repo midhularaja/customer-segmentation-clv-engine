b636707e1ee57a2e5cbbbfd16222e6dd9a88dada
midhularaja <midhularaja2001@gmail.com>
2026-01-29 18:52:20 +0530

    create rfm_segmentaion.py file

---
+++ python/02_rfm_segmentation.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://root:Midhu%4029@localhost/customer_segmentation_db")
+
+fact_sales = pd.read_sql("select * from fact_sales",engine)
+
+fact_sales["InvoiceDate"]=pd.to_datetime(fact_sales["date_key"].astype(str), format="%Y%m%d")
+# set reference date for recency
+analysis_date = fact_sales["InvoiceDate"].max()+pd.Timedelta(days=1)
+
+# calculate rfm matrics
+rfm = fact_sales.groupby("customer_key").agg({
+    "InvoiceDate": lambda x: (analysis_date - x.max()).days,  
+    "InvoiceNo": "nunique",                                  
+    "total_amount": "sum"                                    
+}).reset_index()
+
+rfm.columns = ["customer_key", "Recency", "Frequency", "Monetary"]
+print(rfm.head())
+
+# RFM scoring 1 to 5
+# recency
+rfm["R_Score"] = pd.qcut(rfm["Recency"], 5, labels=[5,4,3,2,1])
+# Frequency
+rfm["F_Score"] = pd.qcut(rfm["Frequency"].rank(method="first"), 5, labels=[1,2,3,4,5])
+# Monetary
+rfm["M_Score"] = pd.qcut(rfm["Monetary"], 5, labels=[1,2,3,4,5])
+
+# Combine into a single RFM Score
+rfm["RFM_Score"] = (
+    rfm["R_Score"].astype(str) +
+    rfm["F_Score"].astype(str) +
+    rfm["M_Score"].astype(str)
+)
+
+print(rfm.head())
+
+# assign segments
+def assign_segment(row):
+    r = int(row["R_Score"])
+    f = int(row["F_Score"])
+    
+    if r >= 4 and f >= 4:
+        return "Champions"
+    elif r <= 2 and f <= 2:
+        return "Hibernating"
+    else:
+        return "Needs Attention"
+# apply segmentation
+rfm['Segment']=rfm.apply(assign_segment,axis=1)
+print(rfm.head())
+
+
+
+
+
