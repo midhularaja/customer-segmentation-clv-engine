e2dbf2ff5dd8a8ce9be64bf57e3c80d7d6df33e1
midhularaja <midhularaja2001@gmail.com>
2026-01-29 22:13:50 +0530

    changes behavioral patterns

--- python/02_rfm_segmentation.py
+++ python/02_rfm_segmentation.py
@@ -21,32 +21,39 @@ print(rfm.head())
 
 # RFM scoring 1 to 5
 # recency
-rfm["R_Score"] = pd.qcut(rfm["Recency"], 5, labels=[5,4,3,2,1])
+rfm["R_Score"] = pd.qcut(rfm["Recency"], 5, labels=[5,4,3,2,1],duplicates="drop")
 # Frequency
 rfm["F_Score"] = pd.qcut(rfm["Frequency"].rank(method="first"), 5, labels=[1,2,3,4,5])
 # Monetary
-rfm["M_Score"] = pd.qcut(rfm["Monetary"], 5, labels=[1,2,3,4,5])
+rfm["M_Score"] = pd.qcut(rfm["Monetary"], 5, labels=[1,2,3,4,5],duplicates="drop")
 
-# Combine into a single RFM Score
-rfm["RFM_Score"] = (
-    rfm["R_Score"].astype(str) +
-    rfm["F_Score"].astype(str) +
-    rfm["M_Score"].astype(str)
-)
-
-print(rfm.head())
 
 # assign segments
 def assign_segment(row):
     r = int(row["R_Score"])
     f = int(row["F_Score"])
+    m = int(row["M_Score"])
     
+    # Behavioral patterns 
     if r >= 4 and f >= 4:
         return "Champions"
+    elif r >= 3 and f >= 3:
+        return "Loyal Customers"
+    elif r >= 4 and f <= 3:
+        return "Potential Loyalist"
+    elif r == 5 and f == 1:
+        return "New Customers"
+    elif r == 4 and f == 1:
+        return "Promising"
+    elif r <= 2 and f >= 3:
+        return "At Risk"
     elif r <= 2 and f <= 2:
         return "Hibernating"
+    elif r == 3 and f <= 2:
+        return "About to Sleep"
     else:
         return "Needs Attention"
+    
 # apply segmentation
 rfm['Segment']=rfm.apply(assign_segment,axis=1)
 print(rfm.head(50))
